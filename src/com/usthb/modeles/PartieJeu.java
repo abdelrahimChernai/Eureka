a54ebd0d9e9acb2f72555ee43919c139d47a5ed0
Abdelrahim Chernai <abdelrahim.chernai@gmail.com>
2020-03-30 18:56:26 +0100

    int the ThemeType enum changed CULTUREGENERALE to CULTURE_GENERALE

--- src/com/usthb/modeles/PartieJeu.java
+++ src/com/usthb/modeles/PartieJeu.java
@@ -11,7 +11,7 @@ public class PartieJeu {
 	protected int number;											//Numéro associé.
 	protected int score;											//Score actuelle.
 	//TODO add question number or question id when clarified.
-	protected ThemeJeu theme;											//Thème sélectionné.
+	protected ThemeJeu theme;										//Thème sélectionné.
 	protected StringBuffer currentCnswer;							//Réponse actuelle Saisie par le joueur.
 }
  
