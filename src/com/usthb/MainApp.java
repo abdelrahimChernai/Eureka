e7c2e5d4bdc28fc69cf16fa8c411a9cc0d810ab8
Abdelrahim Chernai <abdelrahim.chernai@gmail.com>
2020-03-30 03:34:17 +0100

    added documentation for the MainApp class

--- src/com/usthb/MainApp.java
+++ src/com/usthb/MainApp.java
@@ -1,16 +1,26 @@
-/**
- * 
- */
 package com.usthb;
 
 /**
+ * <b>MainApp est la classe contenant le point d'entrée du programme donc elle représente l'application</b>
+ * <p>
+ * 	Dans la MainApp on a :
+ * 		<ul>
+ * 			<li>Une list des joueurs.</li>
+ * 			<li>Une list de thèmes.</li>
+ * 			<li></li>
+ * 		</ul>
+ * </p>
+ * 
+ * @see Joueur
+ * @see ThemJeu
+ * 
  * @author Abdelrahim Chernai
- *
+ * @version 1.0
  */
 public class MainApp {
 
 	/**
-	 * @param args
+	 * @param args - unused.
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
