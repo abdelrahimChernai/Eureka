a134c34046a1d31a4121547d834e2746934372c0
Abdelrahim Chernai <abdelrahim.chernai@gmail.com>
2020-04-01 20:04:14 +0100

    wrote doc for the Joueur calss and all it's instance variables

--- src/com/usthb/modeles/Joueur.java
+++ src/com/usthb/modeles/Joueur.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.usthb.modeles;
 
 import java.util.Date;
@@ -54,11 +51,44 @@ public class Joueur {
 	 * </p>
 	 */
 	protected int currentLvl;			//Dernier niveau atteint
-	protected String lastName;			//Nom du joueur.
-	protected String firstName;			//Prénom du joueur.
-	protected String username;			//Nom d'utilisateur.
-	protected String password;			//Mot de passe.
-	protected Date birthDate;			//Date de naissance.
+	
+	/**
+	 * <p>
+	 * 	Représente le Nom du joueur, est demandé lors de son inscription
+	 * </p>
+	 */
+	protected String lastName;
+	
+	/**
+	 * <p>
+	 * 	Représente le Prénom du joueur, est demandé lors de son inscription
+	 * </p>
+	 */
+	protected String firstName;
+	
+	/**
+	 * <p>
+	 * 	Le nom d'utilisateur à partir du quel on génère un id unique cette
+	 * 	unicité est assuré car lors de la récupération de ce nom d'utilisateur
+	 * 	une génère un id et on vérifie qu'il n'existe pas, si c'est le cas on
+	 *  demande au joueur de donner un autre, est aussi utilisé lors de la
+	 *  connection de joueur.
+	 * </p>
+	 */
+	protected String username;
+	
+	/**
+	 * <p>
+	 * 	On l'utilise lors de la connection du joueur pour s'assurer de son
+	 * 	identité.
+	 * </p>
+	 */
+	protected String password;
+	
+	/**
+	 * La date de naissance du joueur.
+	 */
+	protected Date birthDate;
 	
 	public Joueur() {
 		
