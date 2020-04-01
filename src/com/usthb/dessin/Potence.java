66b2995cf95e55dbfbd998add2df28b3596582b0
Abdelrahim Chernai <abdelrahim.chernai@gmail.com>
2020-04-01 19:44:22 +0100

    wrote doc for the Potance class

--- src/com/usthb/dessin/Potence.java
+++ src/com/usthb/dessin/Potence.java
@@ -1,11 +1,18 @@
-/**
- * 
- */
 package com.usthb.dessin;
 
 /**
+ * <b>S'occupe de la gestion du pendu</b>
+ * 
+ * <p>
+ * 	Garde l'etat du pendu et s'ocupe de le dessiner selon cette état, une
+ * 	Potence est instancié dans la class PartieJeu, voir sa documentation pour
+ * 	plus de détailles.
+ * </p>
+ * 
  * @author Abdelrahim Chernai
- *
+ * @version 1.0
+ * 
+ * @see com.usthb.modeles.PartieJeu
  */
 public class Potence {
 	
