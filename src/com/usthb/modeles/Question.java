e1415943099635555b7c85c714fcd1757e64d001
Abdelrahim Chernai <abdelrahim.chernai@gmail.com>
2020-04-01 16:33:13 +0100

    Revert "Create classes base element"

--- src/com/usthb/modeles/Question.java
+++ src/com/usthb/modeles/Question.java
@@ -5,32 +5,13 @@ package com.usthb.modeles;
 
 /**
  * @author Abdelrahim Chernai
- * @version 2.0
+ *
  */
 public class Question {
 	protected int id;								//Numéro séquentiel automatique.
 	//TODO add question number when clarified.
-	
-	/**
-	 * Le niveaux de la question, ce dernier est associé a un nombre de points
-	 * 
-	 * @see Levels
-	 * @since 2.0
-	 */
-	protected Levels lvl;							
+	protected int lvl;								//Niveau entre 1 et 5 les deux inclues.
 	protected String lable;							//Libellé.
 	protected String answer;						//Réponse.
 	protected String imagePath;						//Lien vers l'image associée.
-
-	/**
-	 * donne le nombre de points associé a une bonne repose à la question
-	 * @return les point associé au niveau voir documentation de Levels pour
-	 *  les valeurs exactes
-	 *  
-	 *  @see Levels
-	 *  @since 2.0
-	 */
-	public int getNumberPoints() {
-		return lvl.lvlPoints;
-	}
 }
--- src/com/usthb/modeles/ThemeJeu.java
+++ src/com/usthb/modeles/ThemeJeu.java
@@ -13,16 +13,6 @@ public class ThemeJeu {
 	protected int coefficent;						//Coefficient.
 	protected String lable;							//Libellé.
 	protected LinkedList <Question> questions;		//Liste des questions.
-	
-	/**
-	 * Le type du thème.
-	 * <p>
-	 * 	Pour plus d'unfirmation sur les valeurs possible voir documentation de
-	 * 	l'enumeration ThemeType
-	 * </p>
-	 * 
-	 * @see ThemeType
-	 */
-	protected ThemeType type;
+	//TODO create an enumeration representing the type instance variable
 	
 }
