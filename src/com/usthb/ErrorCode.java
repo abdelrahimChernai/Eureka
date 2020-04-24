daa2412702ec558e0539517262108465a52105f2
Abdelrahim Chernai <abdelrahim.chernai@gmail.com>
2020-04-24 08:32:29 +0100

    created an Enum to represent the error that happend if one does this will be usfule to comunicate between the controller and the modle

---
+++ src/com/usthb/ErrorCode.java
@@ -0,0 +1,12 @@
+package com.usthb;
+
+public enum ErrorCode {
+	WRONG_USERNAME ("Sure about that?"),
+	WRONG_PASSWORD ("Wrong the password is, my young padawan");
+	
+	String errorMessage;
+	
+	private ErrorCode(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+}
